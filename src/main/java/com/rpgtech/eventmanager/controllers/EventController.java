0ed5961182c228406c78997f8117d750b6fd11ef
Michał Jeniec <72450384+Monti90@users.noreply.github.com>
2022-03-29 13:02:20 +0200

    Created basic EventController and copied its structure to Scenario, Game and User (need to be modified for each of them)

---
+++ src/main/java/com/rpgtech/eventmanager/controllers/EventController.java
@@ -0,0 +1,41 @@
+package com.rpgtech.eventmanager.controllers;
+
+import com.rpgtech.eventmanager.entities.EventEntity;
+import com.rpgtech.eventmanager.services.EventService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/event")
+@AllArgsConstructor
+public class EventController {
+    private EventService eventService;
+
+    @GetMapping
+    public ResponseEntity<List<EventEntity>> getEvents() {return new ResponseEntity<>(eventService.getEvents(), HttpStatus.OK);}
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventEntity> getEventById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(eventService.findEventById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventEntity> addEvent(@RequestBody EventEntity event){
+        return new ResponseEntity<>(eventService.addEvent(event), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventEntity> updateEvent(@PathVariable("id") Long id, @RequestBody EventEntity event){
+        return new ResponseEntity<>(eventService.updateEvent(event, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteEvent(@PathVariable("id") Long id){
+        eventService.deleteEvent(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- src/main/java/com/rpgtech/eventmanager/services/GameService.java
+++ src/main/java/com/rpgtech/eventmanager/services/GameService.java
@@ -1,4 +1,13 @@
 package com.rpgtech.eventmanager.services;
 
+import com.rpgtech.eventmanager.entities.EventEntity;
+
+import java.util.List;
+
 public interface GameService {
+    EventEntity addEvent(EventEntity event);
+    EventEntity updateEvent(EventEntity event, Long id);
+    List<EventEntity> getEvents();
+    EventEntity findEventById(Long id);
+    void deleteEvent(Long id);
 }
